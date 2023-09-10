04239449b9b3a15a7521d9c2ec5fdf103f57239b
Thales Rodrigues <thaleshend@gmail.com>
2023-09-10 17:44:27 -0300

    fix: add ip parameter

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ from Vector import Vector3
 from Trajectory import Trajectory
 
 MULTILANDING = False
+USE_TRAJECTORY = False
+
+if MULTILANDING: USE_TRAJECTORY = False
 
 class LandingBurn:
-    def __init__(self, vessel_tag=None, land_func=None):
-        self.conn = krpc.connect("LandingBurn" + ((": " + vessel_tag) if vessel_tag else ""))
+    def __init__(self, vessel_tag=None, land_func=None, address=None):
+        self.conn = krpc.connect("LandingBurn" + ((": " + vessel_tag) if vessel_tag else ""), address=address if (address is not MULTILANDING and not None) else None)
         self.space_center = self.conn.space_center
         self.vessel = self.space_center.active_vessel
         if vessel_tag:
@@ -18,6 +21,8 @@ class LandingBurn:
                 if len(vessel.parts.with_tag(vessel_tag)) > 0:
                     self.vessel = vessel
                     break
+        self.control = self.vessel.control
+        self.auto_pilot = self.vessel.auto_pilot
         self.body = self.vessel.orbit.body
         self.body_ref = self.body.reference_frame
         self.surface_ref = self.vessel.surface_reference_frame
@@ -36,6 +41,7 @@ class LandingBurn:
         self.stream_position_body = self.conn.add_stream(self.vessel.position, self.body_ref)
         self.stream_surface_altitude = self.conn.add_stream(getattr, self.flight_body, "surface_altitude")
         self.stream_pitch = self.conn.add_stream(getattr, self.flight_surface, "pitch")
+        self.stream_situation = self.conn.add_stream(getattr, self.vessel, "situation")
 
         # ===================
         if land_func is None:
@@ -49,9 +55,11 @@ class LandingBurn:
 
         # consts
         self.a_g = self.body.surface_gravity
-        
+        self.landed_situation = self.vessel.situation.landed
+        self.splashed_situation = self.vessel.situation.splashed
+
         # Params
-        self.tilted_engines = False
+        self.tilted_engines = True
         self.gear_delay = 4
         self.max_twr = 4
         self.eng_threshold = .9
@@ -59,30 +67,28 @@ class LandingBurn:
         self.final_altitude = 5
 
         # Initializing
-        self.vessel.control.throttle = 0
-        self.vessel.control.brakes = True
-        self.vessel.control.rcs = True
-        self.vessel.auto_pilot.engage()
-        self.vessel.auto_pilot.reference_frame = self.body_ref
-        self.vessel.auto_pilot.stopping_time = (0.5, 0.5, 0.5)
-        self.vessel.auto_pilot.deceleration_time = (5, 5, 5)
-        self.vessel.auto_pilot.target_roll = -90
+        self.control.throttle = 0
+        self.control.brakes = True
+        self.control.rcs = True
+        self.auto_pilot.engage()
+        self.auto_pilot.reference_frame = self.body_ref
+        self.auto_pilot.stopping_time = (0.5, 0.5, 0.5)
+        self.auto_pilot.deceleration_time = (5, 5, 5)
+        self.auto_pilot.target_roll = -90
 
         # Waiting
         while self.stream_vel()[0] > 0:
-            self.vessel.auto_pilot.target_direction = self.space_center.transform_direction((1, 0, 0), self.surface_ref, self.body_ref)
+            self.auto_pilot.target_direction = self.space_center.transform_direction((1, 0, 0), self.surface_ref, self.body_ref)
             sleep(0.5)
         
         while self.stream_surface_altitude() > 15000:
-            self.vessel.auto_pilot.target_direction = Vector3(self.flight_body.velocity) * -1
+            self.auto_pilot.target_direction = Vector3(self.flight_body.velocity) * -1
             sleep(0.5)
 
         # Trajectory
-        if not MULTILANDING:
+        if USE_TRAJECTORY:
             self.trajectory = Trajectory()
-
-        # Initializing Trajectory module
-        if not MULTILANDING: self.trajectory.start()
+            self.trajectory.start()
 
         # Thrust correction
         self.thrust = 0
@@ -122,12 +128,12 @@ class LandingBurn:
                 
                 # Throttle Controller
                 if pitch > 0:
-                    if MULTILANDING or vel.y**2 + vel.z**2 < 900:
+                    if USE_TRAJECTORY and vel.y**2 + vel.z**2 > 900:
+                        dist = Vector3(self.stream_position_body()).distance(self.trajectory.land_pos)
+                    else:
                         t_free_fall = (vel.x + sqrt(vel.x**2 + 2*self.a_g*alt)) / self.a_g
                         dist = Vector3(-alt, vel.y*t_free_fall, vel.z*t_free_fall).magnitude()
-                        if not MULTILANDING: self.trajectory.end()
-                    else:
-                        dist = Vector3(self.stream_position_body()).distance(self.trajectory.land_pos)
+                        self.end_trajectory()
 
                     if alt > self.final_altitude:
                         target_speed = sqrt(self.final_speed**2 + 2*a_net*abs(dist-self.final_altitude))
@@ -139,28 +145,28 @@ class LandingBurn:
                         throttle = (delta_speed*2 + self.a_g) / a_eng
 
                         # Check Landed
-                        if self.vessel.situation == self.vessel.situation.landed or self.vessel.situation == self.vessel.situation.splashed:
+                        situation = self.stream_situation()
+                        if situation == self.landed_situation or situation == self.splashed_situation:
                             self.land_func(self)
                             break
 
-                    self.vessel.control.throttle = throttle
+                    self.control.throttle = throttle
             
                     #print(f'Alt: {alt:.2f} | Delta: {delta_speed:.2f} | TFall: {t_fall:.2f}')
 
                     # Check Gears
-                    if t_fall < self.gear_delay: self.vessel.control.gear = True
+                    if t_fall < self.gear_delay: self.control.gear = True
                 else:
-                    self.vessel.control.throttle = 0
+                    self.control.throttle = 0
 
                 # Aim Vessel
-                self.vessel.auto_pilot.target_direction = self.space_center.transform_direction(target_dir, self.surface_ref, self.body_ref)
+                self.auto_pilot.target_direction = self.space_center.transform_direction(target_dir, self.surface_ref, self.body_ref)
         
         except KeyboardInterrupt as e:
-            if not MULTILANDING:
-                self.trajectory.end()
+            self.end_trajectory()
 
     def end_trajectory(self):
-        if not MULTILANDING: self.trajectory.end()
+        if USE_TRAJECTORY: self.trajectory.end()
 
     def progressive_engine_cut(self):
         a_eng = (self.thrust if self.tilted_engines else self.stream_av_thrust()) / self.stream_mass()
@@ -173,28 +179,28 @@ class LandingBurn:
 
         while throttle > 0:
             throttle -= delta_throttle
-            self.vessel.control.throttle = throttle
+            self.control.throttle = throttle
             sleep(dt)
 
-        self.vessel.control.throttle = 0
+        self.control.throttle = 0
 
     def sas_aim_up(self):
-        self.vessel.auto_pilot.disengage()
-        self.vessel.control.rcs = True
-        self.vessel.control.sas = True
+        self.auto_pilot.disengage()
+        self.control.rcs = True
+        self.control.sas = True
         sleep(0.1)
         try:
-            self.vessel.control.sas_mode = self.vessel.control.sas_mode.radial
+            self.control.sas_mode = self.control.sas_mode.radial
         except:
-            self.vessel.control.sas = False
-            self.vessel.auto_pilot.engage()
-            self.vessel.auto_pilot.target_direction = self.space_center.transform_direction((1, 0, 0), self.surface_ref, self.body_ref)
+            self.control.sas = False
+            self.auto_pilot.engage()
+            self.auto_pilot.target_direction = self.space_center.transform_direction((1, 0, 0), self.surface_ref, self.body_ref)
             input("Press enter to finish program.")
-            self.vessel.auto_pilot.disengage()
+            self.auto_pilot.disengage()
 
     def finish(self):
-        self.vessel.control.rcs = True
-        self.vessel.control.brakes = False
+        self.control.rcs = True
+        self.control.brakes = False
         print(f"{self.vessel.name} has landed!")
         self.conn.close()
 
@@ -205,7 +211,12 @@ class LandingBurn:
 if __name__ == "__main__":
     if MULTILANDING:
         from threading import Thread
-
         Thread(target=LandingBurn, args=["sb1"]).start()
         Thread(target=LandingBurn, args=["sb2"]).start()
-    LandingBurn()
+
+    def land_func(connection):
+        connection.control.throttle = 1
+        connection.control.activate_next_stage()
+        connection.finish()
+
+    LandingBurn(address="192.168.50.114")
